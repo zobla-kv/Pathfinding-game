424302a733bd3cef7e39e71f0bcfb0c6057f9c52
zobla <zobla.kv@gmail.com>
2025-09-25 00:00:50 +0200

    Updates

---
+++ precache-manifest.c38e69c1b1774064a2a13b7a43c8ad6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47985172d0653dd6d20b87398d9ddf2d",
+    "url": "/Pathfinding-game/index.html"
+  },
+  {
+    "revision": "4ce687f84933ed5268df",
+    "url": "/Pathfinding-game/static/js/2.ac9ae416.chunk.js"
+  },
+  {
+    "revision": "e939945e62d78c11ba713f1fbef69a8d",
+    "url": "/Pathfinding-game/static/js/2.ac9ae416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03491e5b4a428fffb3de",
+    "url": "/Pathfinding-game/static/js/main.b70278f5.chunk.js"
+  },
+  {
+    "revision": "59544eba3fc7eea60650",
+    "url": "/Pathfinding-game/static/js/runtime-main.0aed432f.js"
+  }
+]);
